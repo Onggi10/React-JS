da43cc77536e1497a49022533eba05d44b2ec0a7
Onggi Sanjaya <onggisanjaya@gmail.com>
2024-11-04 16:41:41 +0700

    Belajar React Js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import "./App.css";
 import React, { Component } from "react";
 import Greet from "./Components/Greet";
 import Welcome from "./Components/Welcome";
+import Love from './Components/Love'
+
 
 function App() {
   return (
     <div className="App">
       <Greet />
+      <Love />
       <Welcome />
     </div>
   );
